3d06c09b66ca5d37b16fa8ef23ccd724c4acf858
Adebola Omoboya <adeomoboya@gmail.com>
2020-04-25 02:10:28 +0100

    fix minor errors with order date display

--- controllers/orders.js
+++ controllers/orders.js
@@ -145,7 +145,7 @@ exports.manageOrders = (req, res, next) => {
 
   // Load Orders from the Database
 
-  Order.find({$query: {}, $orderby: {date: -1}})
+  Order.find({$query: {}, $orderby: {date: 1}})
     .populate("user")
     .lean()
     .skip(perPage * page - perPage)
